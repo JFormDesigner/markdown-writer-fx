e2e1115d1015d39fdafaba5ab80f03e9c3dd43a3
Karl Tauber <karl@jformdesigner.com>
2018-11-20 11:26:10 +0100

    flexmark: use ext-tables extension instead of deprecated ext-gfm-tables extension

--- src/test/java/org/markdownwriterfx/FlexMarkExtensionsTest.java
+++ src/test/java/org/markdownwriterfx/FlexMarkExtensionsTest.java
@@ -43,8 +43,8 @@ public class FlexMarkExtensionsTest
 		com.vladsch.flexmark.ext.definition.DefinitionExtension.create();
 		com.vladsch.flexmark.ext.footnotes.FootnoteExtension.create();
 		com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension.create();
-		com.vladsch.flexmark.ext.gfm.tables.TablesExtension.create();
 		com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension.create();
+		com.vladsch.flexmark.ext.tables.TablesExtension.create();
 		com.vladsch.flexmark.ext.toc.TocExtension.create();
 		com.vladsch.flexmark.ext.wikilink.WikiLinkExtension.create();
 		com.vladsch.flexmark.ext.yaml.front.matter.YamlFrontMatterExtension.create();
